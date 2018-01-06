af58b18472940d2f7e789d14db5f2d7327e1304a
Cyborger <joe.zlonicky@gmail.com>
2018-01-05 18:06:36 -0800

    Added

--- projectile.py
+++ projectile.py
@@ -1,6 +1,16 @@
 import zedlib
+import pygame
+import math
 
 
-class Projectile():
-    def __init__(self):
-        pass
+class Projectile(zedlib.GameSprite):
+    def __init__(self, x, y, angle_radians):
+        super().__init__(pygame.Surface((5, 5)), x, y)
+        self.image.fill((255, 255, 255))
+        self.speed = 6
+        self.angle = angle_radians
+        self.calculate_velocity()
+
+    def calculate_velocity(self):
+        self.x_velocity = math.cos(self.angle) * self.speed
+        self.y_velocity = math.sin(self.angle) * self.speed

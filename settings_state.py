da4063a35a8aeefca4afd81a4c9b7c3069bbc8b8
Cyborger <joe.zlonicky@gmail.com>
2018-01-05 18:07:14 -0800

    Updated to zedlib changes

--- settings_state.py
+++ settings_state.py
@@ -8,10 +8,11 @@ class SettingsState(zedlib.GameState):
         background_image = zedlib.load_image("Resources/Images/"\
                                              "SettingsMenu.png", scale=4)
         self.background = zedlib.Surface(background_image)
-        self.background.center_horizontal(self.game.get_screen_rect())
-        self.background.center_vertical(self.game.get_screen_rect())
+        self.background.center_horizontal(self.game.screen.get_rect())
+        self.background.center_vertical(self.game.screen.get_rect())
 
-    def handle_other_events(self, events):
+    def handle_events(self, events):
+        super().handle_events(events)
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
@@ -19,7 +20,7 @@ class SettingsState(zedlib.GameState):
 
     def update(self):
         self.game.gui.update()
-        
+
     def draw_screen(self):
         self.game.playing_state.draw_screen()
         self.background.draw(self.game.screen)
